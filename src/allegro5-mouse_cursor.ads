bcbce4f735efe55e58fdf62d6ba0a5b400534c00
Oliver Kleinke <oliver@c-01a.de>
2013-07-29 23:21:16 +0200

    adds inline doc for mouse_cursor

--- src/allegro5-mouse_cursor.ads
+++ src/allegro5-mouse_cursor.ads
@@ -1,19 +1,18 @@
-with Interfaces.C; use Interfaces.C;
+with Interfaces.C;            use Interfaces.C;
 with Interfaces.C.Extensions;
 with System;
 
 with Allegro5.Bitmap;
 with Allegro5.Display;
 
-use Allegro5;
-
+use  Allegro5;
 
 package Allegro5.Mouse_Cursor is
 
-   type ALLEGRO_MOUSE_CURSOR is new System.Address;
+   type ALLEGRO_MOUSE_CURSOR is private;
 
-   type ALLEGRO_SYSTEM_MOUSE_CURSOR is 
-     (ALLEGRO_SYSTEM_MOUSE_CURSOR_NONE,
+   type ALLEGRO_SYSTEM_MOUSE_CURSOR is (
+      ALLEGRO_SYSTEM_MOUSE_CURSOR_NONE,
       ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT,
       ALLEGRO_SYSTEM_MOUSE_CURSOR_ARROW,
       ALLEGRO_SYSTEM_MOUSE_CURSOR_BUSY,
@@ -36,25 +35,70 @@ package Allegro5.Mouse_Cursor is
       ALLEGRO_NUM_SYSTEM_MOUSE_CURSORS);
    pragma Convention (C, ALLEGRO_SYSTEM_MOUSE_CURSOR);
 
+   -- Create a mouse cursor from the bitmap provided.
+   --
+   -- Returns a pointer to the cursor on success, or NULL on failure.
    function al_create_mouse_cursor
      (sprite : Bitmap.ALLEGRO_BITMAP;
       xfocus : int;
-      yfocus : int) return ALLEGRO_MOUSE_CURSOR;
+      yfocus : int)
+      return   ALLEGRO_MOUSE_CURSOR;
    pragma Import (C, al_create_mouse_cursor, "al_create_mouse_cursor");
 
+   -- Free the memory used by the given cursor.
+   --
+   -- Has no effect if cursor is NULL.
    procedure al_destroy_mouse_cursor (arg1 : ALLEGRO_MOUSE_CURSOR);
    pragma Import (C, al_destroy_mouse_cursor, "al_destroy_mouse_cursor");
 
-   function al_set_mouse_cursor (display : Allegro5.Display.ALLEGRO_DISPLAY; cursor : ALLEGRO_MOUSE_CURSOR) return Extensions.bool;
+   -- Set the given mouse cursor to be the current mouse cursor for the given
+   --display.
+   --
+   -- If the cursor is currently 'shown' (as opposed to 'hidden') the change
+   --is immediately visible.
+   --
+   -- Returns true on success, false on failure.
+   function al_set_mouse_cursor
+     (display : Allegro5.Display.ALLEGRO_DISPLAY;
+      cursor  : ALLEGRO_MOUSE_CURSOR)
+      return    Extensions.bool;
    pragma Import (C, al_set_mouse_cursor, "al_set_mouse_cursor");
 
-   function al_set_system_mouse_cursor (display : Allegro5.Display.ALLEGRO_DISPLAY; cursor_id : ALLEGRO_SYSTEM_MOUSE_CURSOR) return Extensions.bool;
-   pragma Import (C, al_set_system_mouse_cursor, "al_set_system_mouse_cursor");
+   -- Set the given system mouse cursor to be the current mouse cursor for the
+   --given display. If the cursor is currently 'shown' (as opposed to
+   --'hidden') the change is immediately visible.
+   --
+   -- If the cursor doesn't exist on the current platform another cursor will
+   --be silently be substituted.
+   function al_set_system_mouse_cursor
+     (display   : Allegro5.Display.ALLEGRO_DISPLAY;
+      cursor_id : ALLEGRO_SYSTEM_MOUSE_CURSOR)
+      return      Extensions.bool;
+   pragma Import
+     (C,
+      al_set_system_mouse_cursor,
+      "al_set_system_mouse_cursor");
 
-   function al_show_mouse_cursor (display : Allegro5.Display.ALLEGRO_DISPLAY) return Extensions.bool;
+   -- Make a mouse cursor visible in the given display.
+   --
+   -- Returns true if a mouse cursor is shown as a result of the call (or one
+   --already was visible), false otherwise.
+   function al_show_mouse_cursor
+     (display : Allegro5.Display.ALLEGRO_DISPLAY)
+      return    Extensions.bool;
    pragma Import (C, al_show_mouse_cursor, "al_show_mouse_cursor");
 
-   function al_hide_mouse_cursor (display : Allegro5.Display.ALLEGRO_DISPLAY) return Extensions.bool;
+   -- Hide the mouse cursor in the given display. This has no effect on what
+   --the current mouse cursor looks like; it just makes it disappear.
+   --
+   -- Returns true on success (or if the cursor already was hidden), false
+   --otherwise.
+   function al_hide_mouse_cursor
+     (display : Allegro5.Display.ALLEGRO_DISPLAY)
+      return    Extensions.bool;
    pragma Import (C, al_hide_mouse_cursor, "al_hide_mouse_cursor");
+
+private
+   type ALLEGRO_MOUSE_CURSOR is new System.Address;
 
 end Allegro5.Mouse_Cursor;
